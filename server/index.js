91b6f0ad5d021df4dc13d6d85e68f59cc16bd0bb
Cole Henderson <darkgale93@gmail.com>
2022-04-14 11:46:08 -0600

    created simple server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
